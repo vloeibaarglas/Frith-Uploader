425e1397d6d3ff4e9ce681aceb328e2dfa1d0f77
vloei <vloeibaarglas@gmail.com>
2026-08-13 21:20:24 +0000

    v2.0.0: bump version for Frith rebrand

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.min.frith.uploader"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "2.0.0"
     }
 
     signingConfigs {
